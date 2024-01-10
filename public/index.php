73a15ec1893f946a6fe4729d39bed134f27235a3
Alercio Borgess <alercioborges@hotmail.com>
2024-01-10 17:14:02 -0300

    10-01-2024

--- public/index.php
+++ public/index.php
@@ -4,27 +4,22 @@ require "../bootstrap.php";
 
 $app->get('', 'app\controllers\OverviewController:index');
 
-$app->get('/users', 'app\controllers\UserController:show');
 
-$app->get('/users/{id:[0-9]+}', 'app\controllers\UserController:edit');
+$app->group('/users', function (Slim\Routing\RouteCollectorProxy $group) {
 
-$app->post('/users/update', 'app\controllers\UserController:update');
+	$group->get('', 'app\controllers\UserController:show');
+	$group->get('/{id:[0-9]+}', 'app\controllers\UserController:edit');	
+	$group->get('/create', 'app\controllers\UserController:create');	
+	$group->get('/{id:[0-9]+}/profile', 'app\controllers\UserController:profile');	
+	$group->get('/{id:[0-9]+}/suspend', 'app\controllers\UserController:suspend');
+	$group->get('/{id:[0-9]+}/unsuspend', 'app\controllers\UserController:unsuspend');
+	$group->get('/{id:[0-9]+}/delete', 'app\controllers\UserController:delete');
 
-$app->get('/users/create', 'app\controllers\UserController:create');
-$app->post('/users/create', 'app\controllers\UserController:save');
+	$group->post('/update', 'app\controllers\UserController:update');
+	$group->post('/create', 'app\controllers\UserController:save');
+	$group->post('/{id:[0-9]+}/reset-password', 'app\controllers\UserController:resetPassword');
 
-
-$app->get('/users/{id:[0-9]+}/profile', 'app\controllers\UserController:profile');
-
-$app->post('/users/{id:[0-9]+}/reset-password', 'app\controllers\UserController:resetPassword');
-
-
-
-$app->get('/users/{id:[0-9]+}/suspend', 'app\controllers\UserController:suspend');
-$app->get('/users/{id:[0-9]+}/unsuspend', 'app\controllers\UserController:unsuspend');
-
-$app->get('/users/{id:[0-9]+}/delete', 'app\controllers\UserController:delete');
-
+});
 
 $app->get('/course-categories', 'app\controllers\CourseCategoryController:show');
 $app->get('/course-categories/create', 'app\controllers\CourseCategoryController:create');
