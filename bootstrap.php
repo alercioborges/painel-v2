5b10b138a95d74b91f59137d04f5208864dbc763
alercioborges <alercio.borges@hotmail.com>
2025-07-11 17:35:39 -0300

    11-07-2025

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,6 @@ use Slim\Factory\AppFactory;
 use Dotenv\Dotenv;
 use Zeuxisoo\Whoops\Slim\WhoopsMiddleware;
 
-use app\Config;
 use app\src\Middleware;
 
 if (session_status() !== PHP_SESSION_ACTIVE) {
@@ -17,16 +16,26 @@ require __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+// Criar aplicação Slim
 $app = AppFactory::create();
 
-$app->setBasePath(Config::BASE_DIR);
+// Adicionando diretório do dominio
+if (isset($_ENV['APP_BASE_DIR'])) {
+	$app->setBasePath($_ENV['APP_BASE_DIR']);
+}
 
+// Adiciona um Middleware as rotas
 $app->addRoutingMiddleware();
 
-$app->add(new TrailingSlash(false));
+// Middleware para trailing slash
+$app->add(new TrailingSlash(false)); // Adiciona barra ao final
 
-$app->add(new WhoopsMiddleware());
+// Configurar Whoops para debug (apenas em desenvolvimento)
+if (isset($_ENV['APP_ENV']) && $_ENV['APP_ENV'] === 'development') {
+	$app->add(new WhoopsMiddleware());
+}
 
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+// Configurar roteamento
+$errorMiddleware = $app->addErrorMiddleware($_ENV['APP_DEBUG'], true, true);
 
 $middleware = new Middleware();
